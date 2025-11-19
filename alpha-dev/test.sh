d930699074e4d3b88e4ce3a88abfc37778b99669
Victor Shevtsov <victorshevtsov@gmail.com>
2025-11-19 10:19:56 +0000

    Add alpha-dev version to perform tests for the latest dev branch of TLSN

---
+++ alpha-dev/test.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+set -e
+
+cargo build --release --examples
+
+export RUST_LOG=none
+
+URI=https://mock.verity.usher.so/1kb.json ./target/release/examples/prove
+URI=https://mock.verity.usher.so/2kb.json ./target/release/examples/prove
+URI=https://mock.verity.usher.so/4kb.json ./target/release/examples/prove
+URI=https://mock.verity.usher.so/8kb.json ./target/release/examples/prove
+URI=https://mock.verity.usher.so/16kb.json ./target/release/examples/prove 
+URI=https://mock.verity.usher.so/32kb.json ./target/release/examples/prove
+URI=https://mock.verity.usher.so/64kb.json ./target/release/examples/prove
+# URI=https://mock.verity.usher.so/128kb.json ./target/release/examples/prove
+# URI=https://mock.verity.usher.so/256kb.json ./target/release/examples/prove
+# URI=https://mock.verity.usher.so/512kb.json ./target/release/examples/prove
+# URI=https://mock.verity.usher.so/1024kb.json ./target/release/examples/prove
